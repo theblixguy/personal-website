7c746b1ce525e04f244e0976d61608ca64928bd4
Suyash Srijan <suyashsrijan@outlook.com>
2025-02-08 23:00:07 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+let baseUrl = '/'
+if (process.env.PREVIEW_PATH) {
+  baseUrl = `/personal-website/${process.env.PREVIEW_PATH}/`
+}
+
 export default defineConfig({
+  base: baseUrl,
   plugins: [react()]
 })
